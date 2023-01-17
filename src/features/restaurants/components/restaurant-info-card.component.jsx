aa78a824076bd27627e75ef963a9a7c21a093014
lovenofill <lovenofill@gmail.com>
2023-01-17 14:27:31 +0700

    SVG

--- src/features/restaurants/components/restaurant-info-card.component.jsx
+++ src/features/restaurants/components/restaurant-info-card.component.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import { Card } from "react-native-paper";
 import styled from "styled-components/native";
-import { View, Text } from "react-native";
-
+import { Text } from "react-native";
+import { SvgXml } from "react-native-svg";
+import star from "../../../../assets/star";
 const RestaurantCard = styled(Card)`
   background-color: ${(props) => props.theme.colors.bg.secondary};
 `;
@@ -27,6 +28,12 @@ const Info = styled.View`
   padding: ${(props) => props.theme.space[3]};
 `;
 
+const Rating = styled.View`
+  flex-direction: row;
+  padding-top: ${(props) => props.theme.space[2]};
+  padding-bottom: ${(props) => props.theme.space[2]};
+`;
+
 export const RestaurantInfoCard = ({ ...restaurant }) => {
   const {
     name = "Some Restaurant",
@@ -40,12 +47,19 @@ export const RestaurantInfoCard = ({ ...restaurant }) => {
     isClosedTemporarily,
   } = restaurant;
 
+  const ratingArray = Array.from(new Array(Math.floor(rating)));
+
   return (
     <RestaurantCard elevation={5}>
       <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
       <Info>
         <Title>{name}</Title>
         <Address>{address}</Address>
+        <Rating>
+          {ratingArray.map((_, index) => (
+            <SvgXml key={index} xml={star} width={20} height={20} />
+          ))}
+        </Rating>
       </Info>
     </RestaurantCard>
   );
